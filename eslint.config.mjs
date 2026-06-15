1b53ac076e6e003b0650551d14a8c646ef8df40f
Helloyunho <me@helloyunho.xyz>
2026-06-16 00:14:10 +0900

    feat: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import neostandard from 'neostandard'
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  ...neostandard({
+    ts: true
+  }),
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+
+export default eslintConfig
